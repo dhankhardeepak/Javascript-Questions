bc33d4255d427c3e794374e926daec4297d2af26
Singh <ddhankhar18061995@gmail.com>
2026-04-25 15:14:51 +0530

    comitting memoize funciton

---
+++ Memoize function/Memoize.js
@@ -0,0 +1,17 @@
+/**
+ * @param {(arg: unknown) => unknown} func
+ * @returns {(arg: unknown) => unknown}
+ */
+
+
+export default function memoize(func) {
+  const cache = new Map();
+  return function(arg){
+    if(cache.has(arg)){
+      return cache.get(arg);
+    }
+    const result = func.call(this, arg);
+    cache.set(arg, result);
+    return result;
+  }
+}
